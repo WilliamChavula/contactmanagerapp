162be11673f5abee9cf77ef682233aabf5e83d4d
WilliamChavula <rumbani@gmail.com>
2020-10-13 14:34:53 -0500

    edit branding link to point to homepage

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header = (props) => {
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
             <div className="container">
-                <a href="https://WilliamChavula.github.io/contactmanagerapp" className="navbar-brand">{branding}</a>
+                <a href="https://WilliamChavula.github.io/contactmanagerapp/#/" className="navbar-brand">{branding}</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
