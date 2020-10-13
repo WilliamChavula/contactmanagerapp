0a4cd71fd9d496390f7d4e638c5d38e82782e9d4
WilliamChavula <rumbani@gmail.com>
2020-10-13 14:17:56 -0500

    Updates

---
+++ precache-manifest.7e7318e3713e7eba285d2cea7a60d182.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc0044886faafa4d5abc397bfc985f95",
+    "url": "/contactmanagerapp/index.html"
+  },
+  {
+    "revision": "7cc9dde03f5de378dea4",
+    "url": "/contactmanagerapp/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "71a09919c7d81dfe9dde",
+    "url": "/contactmanagerapp/static/css/main.6c642913.chunk.css"
+  },
+  {
+    "revision": "7cc9dde03f5de378dea4",
+    "url": "/contactmanagerapp/static/js/2.7f062d7c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanagerapp/static/js/2.7f062d7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71a09919c7d81dfe9dde",
+    "url": "/contactmanagerapp/static/js/main.1428154f.chunk.js"
+  },
+  {
+    "revision": "0b2ad555a539cbfc8dba",
+    "url": "/contactmanagerapp/static/js/runtime-main.fd6e7ea8.js"
+  },
+  {
+    "revision": "05761613d96ed564842fa726753c18cc",
+    "url": "/contactmanagerapp/static/media/pagenotfound.05761613.png"
+  }
+]);
